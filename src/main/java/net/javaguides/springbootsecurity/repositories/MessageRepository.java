afcb71a0ed4ecad302e6a38b168a5a12736dcc53
Caio Figueiredo <caiofernandomf@gmail.com>
2020-11-08 00:35:27 -0300

    remove a method

--- src/main/java/net/javaguides/springbootsecurity/repositories/MessageRepository.java
+++ src/main/java/net/javaguides/springbootsecurity/repositories/MessageRepository.java
@@ -1,17 +1,15 @@
 package net.javaguides.springbootsecurity.repositories;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import net.javaguides.springbootsecurity.entities.Message;
 import net.javaguides.springbootsecurity.entities.User;
 
 /**
  * @author Caio Fernando
  */
-public interface MessageRepository extends JpaRepository<Message,Integer>{
-    Optional <User> findUserByEmail(String email);
+public interface MessageRepository extends JpaRepository<Message,Integer>
+{
+    
     List<Message> findByUser(User user);    
 }
