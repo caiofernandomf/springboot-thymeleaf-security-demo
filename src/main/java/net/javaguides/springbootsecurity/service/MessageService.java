3ea2d4e4adeedaf5c5b38a7838c36bc6d71c4e87
Caio Figueiredo <caiofernandomf@gmail.com>
2020-11-07 22:48:33 -0300

    Adding messageList method to show messages from user logged

--- src/main/java/net/javaguides/springbootsecurity/service/MessageService.java
+++ src/main/java/net/javaguides/springbootsecurity/service/MessageService.java
@@ -1,8 +1,12 @@
 package net.javaguides.springbootsecurity.service;
 
+import java.security.Principal;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import net.javaguides.springbootsecurity.entities.Message;
 import net.javaguides.springbootsecurity.repositories.MessageRepository;
 
 @Service
@@ -10,4 +14,15 @@ public class MessageService {
     
     @Autowired
     private MessageRepository messageRepository;
+
+    public List<Message> messageList(Principal principal){
+                
+        return messageRepository.
+                findByUser(
+                    messageRepository.
+                    findUserByEmail(
+                        principal.getName()
+                    ).get()
+                );
+    }
 }
