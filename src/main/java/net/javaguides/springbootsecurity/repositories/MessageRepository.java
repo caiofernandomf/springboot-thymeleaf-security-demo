d219ee2781a074029569435888bca155f9ef1250
Caio Figueiredo <caiofernandomf@gmail.com>
2020-11-07 22:46:32 -0300

    Adding methods to find user by email and messages from user

--- src/main/java/net/javaguides/springbootsecurity/repositories/MessageRepository.java
+++ src/main/java/net/javaguides/springbootsecurity/repositories/MessageRepository.java
@@ -1,12 +1,17 @@
 package net.javaguides.springbootsecurity.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.javaguides.springbootsecurity.entities.Message;
+import net.javaguides.springbootsecurity.entities.User;
 
 /**
  * @author Caio Fernando
  */
 public interface MessageRepository extends JpaRepository<Message,Integer>{
-
+    Optional <User> findUserByEmail(String email);
+    List<Message> findByUser(User user);    
 }
