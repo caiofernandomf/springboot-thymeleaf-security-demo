36fa847d91060b64a05e1d910fd8f7d3986f2948
Caio Figueiredo <caiofernandomf@gmail.com>
2020-11-07 22:58:18 -0300

    change to MessageService file

--- src/main/java/net/javaguides/springbootsecurity/web/HomeController.java
+++ src/main/java/net/javaguides/springbootsecurity/web/HomeController.java
@@ -9,29 +9,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import net.javaguides.springbootsecurity.entities.Message;
-import net.javaguides.springbootsecurity.repositories.MessageRepository;
+import net.javaguides.springbootsecurity.service.MessageService;
 
 /**
  * @author Caio Fernando
  */
 
  @Controller
-public class HomeController {
+public class HomeController {    
     
     @Autowired
-    private MessageRepository messageRepository;
+    private MessageService messageService;
 
     @GetMapping("/home")
-    public String home(Model model){
-
-        model.addAttribute("msgs",messageRepository.findAll());
+    public String home(Principal principal,Model model){
+        model.addAttribute("msgs",messageService.messageList(principal));
         return "userhome";
     }
 
     @PostMapping("/messages")
     public String saveMessage(Principal principal
                             ,Message message){        
-        messageRepository.save(message);        
+        messageService.saveMessage(principal, message);
         return "redirect:/home";
     }
 }
